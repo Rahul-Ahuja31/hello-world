4a0727625bebe5dac6fd1a6e7fc590f2734059fa
Rahul-Ahuja31 <rahulahuja131@gmail.com>
2022-01-03 16:15:22 +0530

    Hello World

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Greet from './components/Greet';
+import { Component } from 'react';
+import Welcome from './components/Welcome';
+import Message from './components/Message';
+import Counter from './components/Counter';
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        {/* <Counter/> */}
+        <Message/>
+        <Greet name='Rahul'/>
+        <Greet name='Chirag'/>
+      </div>
+    );
+  }
+}
+
+export default App;
